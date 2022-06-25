fd256829f027cf04c1657c97258cfc245ce850cd
Colombiano <lpcolombiano@gmail.com>
2022-06-24 21:00:51 -0300

    realizada código em js, formulário

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,6 @@
+let nome = "Luiz Paulo Colombiano"
+let email = "lpcolombiano@gmail.com"
+let profissão = "servidor público"
+let idade = 39
+let visitar_mundo = "Butão"
+console.log( {nome, email, profissão, idade, visitar_mundo} )
